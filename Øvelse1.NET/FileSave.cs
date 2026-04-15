a6ddf14d98a05d23a3866169a7accb713103aa5f
Jonas Frost <jonasmfrost@gmail.com>
2026-04-15 09:55:53 +0200

    gfgfggfuuu

---
+++ Øvelse1.NET/FileSave.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Filesave
+{
+    public static string Save(string filePath, string content)
+    {
+        File.WriteAllText(filePath, content);
+        return "Fil gemt i din user folder.";
+    }
+
+    public static string Save(string content)
+    {
+        string projectFolder = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName;
+
+        string filePath = Path.Combine(projectFolder, "log.txt");
+
+        File.WriteAllText(filePath, content);
+
+        return "Fil gemt i din konsol app projekt.";
+    }
+}
